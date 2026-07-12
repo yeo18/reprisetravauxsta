846990aba6f24b3860b0080983d5739b92550997
Yeo Tcheregnimin Daniel <tcheregnimdanielyeo@gmail.com>
2026-07-12 18:27:47 +0000

    exception

---
+++ final/src/main/java/com/example/demo/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.demo.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.put("error", "Internal Server Error");
+        response.put("message", ex.getMessage()); // ← maintenant on voit l'erreur
+        response.put("path", "/api/auth/login");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
